0cba424478ae4422f348b4d357ce47afb5d29da0
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-01-19 10:20:24 +0700

    Cập nhập code
    sửa mới và thêm dòng

--- HelloWorld/src/XinChao.java
+++ HelloWorld/src/XinChao.java
@@ -12,7 +12,8 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Tôi lần đầu học JAVA, và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.println("Tôi lần đầu học Java, và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.print("Tôi thêm 1 dòng mới và sửa lại chữ Java ở trên");
 	}
 
 }
