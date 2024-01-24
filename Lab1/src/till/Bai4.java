807b568c8f40ca24d70a3e7c4eba71d65794f8a5
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-01-24 14:40:27 +0700

    Bai4 lab 1

--- Lab1/src/till/Bai4.java
+++ Lab1/src/till/Bai4.java
@@ -1,5 +1,18 @@
 package till;
 
-public class Bai4 {
+import java.util.Scanner;
 
+public class Bai4 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		// Nhập a
+		System.out.print("Nhập hệ số a: ");
+	    double a = scanner.nextDouble();
+	    //Nhập b
+	    System.out.print("Nhập hệ số b: ");
+        double b = scanner.nextDouble();
+        // Nhâp c
+        System.out.print("Nhập hệ số c: ");
+        double c = scanner.nextDouble();
+	}
 }
