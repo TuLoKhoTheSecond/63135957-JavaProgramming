e2fe9f8c8afeb55b059c404937be01c9b221621d
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-01-24 14:38:39 +0700

    Cập nhập bài 3

--- Lab1/src/till/Bai3.java
+++ Lab1/src/till/Bai3.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 
 public class Bai3 {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		 System.out.print("Nhập độ dài cạnh của khối lập phương: ");
+	     double canh = scanner.nextDouble();
+	     double theTich = Math.pow(canh, 3);
+	     System.out.printf("Thể tích của khối lập phương là: %.2f\n", theTich);
+	     scanner.close();
 	}
 	
 }
