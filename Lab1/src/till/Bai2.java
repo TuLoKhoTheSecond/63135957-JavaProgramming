a7e7e2d9a1499a82e8fb9feea120ea9ba1534f0e
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-01-24 14:29:30 +0700

    Sua bai 2 lan 1

--- Lab1/src/till/Bai2.java
+++ Lab1/src/till/Bai2.java
@@ -1,5 +1,18 @@
 package till;
 
-public class Bai2 {
+import java.util.Scanner;
 
+public class Bai2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		//Tính chiều dài 
+		System.out.print("Nhập chiều dài của hình chữ nhật: ");
+        double dai = scanner.nextDouble();
+        //Tính chiều rộng
+        System.out.print("Nhập chiều rộng của hình chữ nhật: ");
+        double rong = scanner.nextDouble();
+        //Tính chu Vi
+        double chuVi = (dai + rong) * 2;
+        scanner.close();
+	}
 }
