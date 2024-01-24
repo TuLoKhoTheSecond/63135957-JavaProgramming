66536ff7fc3d621c82b9ca28cfd4a8da0c2590e9
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-01-24 14:49:56 +0700

    Hoàn thiện bài

--- Lab2/src/till/Bai1.java
+++ Lab2/src/till/Bai1.java
@@ -1,5 +1,28 @@
 package till;
 
+import java.util.Scanner;
+
 public class Bai1 {
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+		 // Nhập a
+		 System.out.print("Nhập hệ số a: ");
+	     double a = scanner.nextDouble();
+		 // Nhập b
+	     System.out.print("Nhập hệ số b: ");
+	     double b = scanner.nextDouble();
+	     //Hàm giải pt bậc nhất
+	     if (a == 0) {
+	            if (b == 0) {
+	                System.out.println("Phương trình vô số nghiệm.");
+	            } else {
+	                System.out.println("Phương trình vô nghiệm.");
+	            }
+	        } else {
+	            double x = -b / a;
+	            System.out.printf("Nghiệm của phương trình: x = %.2f\n", x);
+	        }
+	     scanner.close();
+	}
 
 }
