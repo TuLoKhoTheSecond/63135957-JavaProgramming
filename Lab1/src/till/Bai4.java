d6d0994456e325c797fbe55565d774f11dedf047
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-01-24 14:44:42 +0700

    Hoàn thiện bài 4

--- Lab1/src/till/Bai4.java
+++ Lab1/src/till/Bai4.java
@@ -14,5 +14,15 @@ public class Bai4 {
         // Nhâp c
         System.out.print("Nhập hệ số c: ");
         double c = scanner.nextDouble();
+        //tính toán
+        double delta = Math.pow(b, 2) - 4 * a * c;
+        //Hàm KT
+        if (delta < 0) {
+            System.out.println("Phương trình vô nghiệm.");
+        } else {
+        	double canDelta = Math.sqrt(delta);
+        	System.out.printf("Căn của delta: %.2f\n", canDelta);
+        }
+        scanner.close();
 	}
 }
