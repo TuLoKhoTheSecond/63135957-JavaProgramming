0398359d7206f9daa69a6571d07585544d264b88
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-01-25 12:05:58 +0700

    update

--- Lab2/src/till/Bai4.java
+++ Lab2/src/till/Bai4.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 
 public class Bai4 {
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		// Nhập SDT
+		System.out.print("Nhập số điện sử dụng trong tháng: ");
+        int soDien = scanner.nextInt();
+        //Hàm tính tiền
+        
+        scanner.close();
 	}
 
 }
