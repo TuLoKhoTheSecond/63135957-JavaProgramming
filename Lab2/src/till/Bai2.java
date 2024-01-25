f7e88daab81e79b029df237cf2128bf64233811b
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-01-25 11:10:03 +0700

    Update Bai2.java

--- Lab2/src/till/Bai2.java
+++ Lab2/src/till/Bai2.java
@@ -1,5 +1,34 @@
 package till;
 
-public class Bai2 {
+import java.util.Scanner;
 
+public class Bai2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		//Nhập a
+		System.out.print("Nhập hệ số a: ");
+        double a = scanner.nextDouble();
+		//Nhập b
+        System.out.print("Nhập hệ số b: ");
+        double b = scanner.nextDouble();
+		//Nhập c
+        System.out.print("Nhập hệ số c: ");
+        double c = scanner.nextDouble();
+		//Hàm KT
+        if (a == 0) {
+            // Hàm giải pt bậc nhất
+            if (b == 0) {
+                if (c == 0) {
+                    System.out.println("Phương trình vô số nghiệm.");
+                } else {
+                    System.out.println("Phương trình vô nghiệm.");
+                }
+            } else {
+                double x = -c / b; //Hàm Giải PT bậc 2
+                System.out.printf("Nghiệm của phương trình: x = %.2f\n", x);
+            }
+        
+     }
+        scanner.close();
+	}
 }
