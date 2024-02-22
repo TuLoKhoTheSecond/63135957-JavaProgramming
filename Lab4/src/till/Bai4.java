243cc627840f1a437a3f39cd3f0dd50ff6f6d743
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-02-22 10:31:31 +0700

    Hoan thien lab4

--- Lab4/src/till/Bai4.java
+++ Lab4/src/till/Bai4.java
@@ -1,5 +1,69 @@
 package till;
 
-public class Bai4 {
+import java.util.Scanner;
 
+public class Bai4 {
+    public static class SanPham {
+        private String tenSp;
+        private double donGia;
+        private double giamGia;
+
+        public SanPham(String tenSp, double donGia, double giamGia) {
+            this.tenSp = tenSp;
+            this.donGia = donGia;
+            this.giamGia = giamGia;
+        }
+
+        public double getThueNhapKhau() {
+            return 0.1 * donGia;
+        }
+
+        public void xuat() {
+            System.out.println("Ten Sp: " + tenSp);
+            System.out.println("Don gia: " + donGia);
+            System.out.println("Giam gia: " + giamGia);
+            System.out.println("Thue nhap khau: " + getThueNhapKhau());
+        }
+
+        public String getTenSp() {
+            return tenSp;
+        }
+
+        public void setTenSp(String tenSp) {
+            this.tenSp = tenSp;
+        }
+
+        public double getDonGia() {
+            return donGia;
+        }
+
+        public void setDonGia(double donGia) {
+            this.donGia = donGia;
+        }
+
+        public double getGiamGia() {
+            return giamGia;
+        }
+
+        public void setGiamGia(double giamGia) {
+            this.giamGia = giamGia;
+        }
+
+        public static void main(String[] args) {
+            SanPham sp = new SanPham("Ten Sp", 0.0, 0.0);
+            sp.nhap();
+            sp.xuat();
+        }
+
+        public void nhap() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Nhap ten sp: ");
+            tenSp = scanner.nextLine();
+            System.out.print("Nhap don gia: ");
+            donGia = scanner.nextDouble();
+            System.out.print("Nhap giam gia: ");
+            giamGia = scanner.nextDouble();
+            scanner.close();
+        }
+    }
 }
