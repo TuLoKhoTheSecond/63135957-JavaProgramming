4c77ef72efc9ebb72ce1e8b252a7ec995a396829
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-02-22 09:18:48 +0700

    hoan thien bai 1

--- Lab3/src/till/Bai1.java
+++ Lab3/src/till/Bai1.java
@@ -1,5 +1,32 @@
 package till;
 
-public class Bai1 {
+import java.util.Scanner;
 
+public class Bai1 {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap 1 so nguyen duong: ");
+        int number = scanner.nextInt();
+        
+        boolean isPrime = true;
+        
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        
+        if (isPrime) {
+            System.out.println(number + " la so NTo");
+        } else {
+            System.out.println(number + " Ko phai so NTo");
+        }
+        
+        scanner.close();
+    }
 }
