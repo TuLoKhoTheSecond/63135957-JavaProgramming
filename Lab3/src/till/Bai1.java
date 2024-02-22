f06d788682403332cecd923bf22b5d42e25ba6b9
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-02-22 09:25:22 +0700

    Hoan thien bai2

--- Lab3/src/till/Bai1.java
+++ Lab3/src/till/Bai1.java
@@ -3,30 +3,30 @@ package till;
 import java.util.Scanner;
 
 public class Bai1 {
-	public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhap 1 so nguyen duong: ");
-        int number = scanner.nextInt();
-        
-        boolean isPrime = true;
-        
-        if (number <= 1) {
-            isPrime = false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(number); i++) {
-                if (number % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-        
-        if (isPrime) {
-            System.out.println(number + " la so NTo");
-        } else {
-            System.out.println(number + " Ko phai so NTo");
-        }
-        
-        scanner.close();
-    }
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Nhap 1 so nguyen duong: ");
+	        int number = scanner.nextInt();
+	        
+	        boolean ok = true;
+	        
+	        if (number <= 1) {
+	            ok = false;
+	        } else {
+	            for (int i = 2; i < number; i++) {
+	                if (number % i == 0) {
+	                    ok = false;
+	                    break;
+	                }
+	            }
+	        }
+	        
+	        if (ok) {
+	            System.out.println(number + " la so Nto");
+	        } else {
+	            System.out.println(number + " Ko phai so Nto");
+	        }
+	        
+	        scanner.close();
+	    }
 }
--- Lab3/src/till/Bai2.java
+++ Lab3/src/till/Bai2.java
@@ -1,5 +1,14 @@
 package till;
 
-public class Bai2 {
+import java.util.Scanner;
 
+public class Bai2 {
+	public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%d x %d = %d\n", i, j, i * j);
+            }
+            System.out.println(); 
+        }
+    }
 }
