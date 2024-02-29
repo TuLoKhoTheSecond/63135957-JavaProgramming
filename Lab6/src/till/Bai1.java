ba7f196958ff471352929399eff2c8ef4709fa52
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-02-29 14:36:02 +0700

    sửa lỗi

--- Lab6/src/till/Bai1.java
+++ Lab6/src/till/Bai1.java
@@ -3,8 +3,8 @@ package till;
 import java.util.Scanner;
 
 public class Bai1 {
-	public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in, "UTF-8");
         System.out.print("Nhập họ và tên: ");
         String hoTen = scanner.nextLine();
 
@@ -13,19 +13,21 @@ public class Bai1 {
         String ten = Ten(hoTen).toUpperCase();
 
         System.out.println("H v T: " + ho + " " + tenDem + " " + ten);
-	}
-	public static String Ho(String hoTen) {
+    }
+
+    public static String Ho(String hoTen) {
         int vTKhoangTrangDau = hoTen.indexOf(" ");
         return hoTen.substring(0, vTKhoangTrangDau);
     }
-	public static String TenDem(String hoTen) {
+
+    public static String TenDem(String hoTen) {
         int vTKhoangTrangDau = hoTen.indexOf(" ");
         int vTKhoangTrangCuoi = hoTen.lastIndexOf(" ");
         return hoTen.substring(vTKhoangTrangDau + 1, vTKhoangTrangCuoi);
     }
+
     public static String Ten(String hoTen) {
         int vTKhoangTrangCuoi = hoTen.lastIndexOf(" ");
         return hoTen.substring(vTKhoangTrangCuoi + 1);
     }
-
 }
