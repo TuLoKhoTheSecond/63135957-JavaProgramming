82ec80ff0b752f483e80d53f34445b1d70243b02
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-02-29 14:31:50 +0700

    bài 1 lab6

--- Lab6/src/till/Bai1.java
+++ Lab6/src/till/Bai1.java
@@ -1,6 +1,31 @@
 package till;
 
+import java.util.Scanner;
+
 public class Bai1 {
-	public 
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập họ và tên: ");
+        String hoTen = scanner.nextLine();
+
+        String ho = Ho(hoTen).toUpperCase();
+        String tenDem = TenDem(hoTen);
+        String ten = Ten(hoTen).toUpperCase();
+
+        System.out.println("H v T: " + ho + " " + tenDem + " " + ten);
+	}
+	public static String Ho(String hoTen) {
+        int vTKhoangTrangDau = hoTen.indexOf(" ");
+        return hoTen.substring(0, vTKhoangTrangDau);
+    }
+	public static String TenDem(String hoTen) {
+        int vTKhoangTrangDau = hoTen.indexOf(" ");
+        int vTKhoangTrangCuoi = hoTen.lastIndexOf(" ");
+        return hoTen.substring(vTKhoangTrangDau + 1, vTKhoangTrangCuoi);
+    }
+    public static String Ten(String hoTen) {
+        int vTKhoangTrangCuoi = hoTen.lastIndexOf(" ");
+        return hoTen.substring(vTKhoangTrangCuoi + 1);
+    }
 
 }
