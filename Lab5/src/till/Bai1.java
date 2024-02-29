e150941f6546653594792f7bb012ecc03b9bc31a
TuLoKhoTheSecond <tu.nna.63cntt@ntu.edu.vn>
2024-02-29 14:02:40 +0700

    hoàn thiện bài 1

--- Lab5/src/till/Bai1.java
+++ Lab5/src/till/Bai1.java
@@ -1,5 +1,33 @@
 package till;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Bai1 {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Double> list = new ArrayList<>();
+
+        System.out.println("Nhập ds số thực (bấm N để kết thúc): ");
+        while (true) {
+            Double x = scanner.nextDouble();
+            list.add(x);
+            System.out.print("Nhập thêm (Y/N)? ");
+            scanner.nextLine(); 
+            if (scanner.nextLine().equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+        System.out.println("Ds số thực đã nhập:");
+        for (Double number : list) {
+            System.out.println(number);
+        }
+
+        double sum = 0;
+        for (Double number : list) {
+            sum += number;
+        }
+        System.out.println("Sum ds số thực: " + sum);
+	}
 
 }
